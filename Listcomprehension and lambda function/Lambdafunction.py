2c8930542385071880fe40b889ccd46c049278a3
Urvashi Shasne <145223767+Urvashishasne@users.noreply.github.com>
2025-07-27 23:32:08 +0530

    Add files via upload

---
+++ Listcomprehension and lambda function/Lambdafunction.py
@@ -0,0 +1,3 @@
+list1 = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+cubes = list(map(lambda x: x**3, list1))
+print(cubes)
---
+++ Listcomprehension and lambda function/list_comprehension_exercise.py
@@ -0,0 +1,6 @@
+inp=[1,2,3,4,5,6,7]
+cube_dict={x:x**3 for x in inp if inp%2!=0}
+
+# qu-2 
+alphabet_dict = {chr(i): i - 96 for i in range(97, 123)}
+print(alphabet_dict)
